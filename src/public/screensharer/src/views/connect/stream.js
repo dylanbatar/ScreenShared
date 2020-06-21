1f50f9d3ffef632b1eec16b5644f7d2045c3aa26
dylanbatar <dylanbatar2@gmail.com>
2020-06-21 13:57:17 -0500

    Solucion de transmicion: ya se refleja la pantalla en otro dispositivo

--- src/public/screensharer/src/views/connect/stream.js
+++ src/public/screensharer/src/views/connect/stream.js
@@ -11,7 +11,9 @@ export default class Stream extends Component {
     this.other = beReceptor(props.location.state.access_code);
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.context = this.video.current.getContext("2d");
+  }
 
   render() {
     const getScreen = async () => {
@@ -22,41 +24,14 @@ export default class Stream extends Component {
 
           if (media) {
             //🚀
-            this.context = this.video.current.getContext("2d");
-
-            console.log(media);
-
-            // let arrayBufferView = new Uint8Array(media);
-            // let blob = new Blob([arrayBufferView], { type: "image/webp" });
-            // let urlCreator = window.URL || window.webkitURL;
-            // let imageUrl = urlCreator.createObjectURL(blob);
-
-            // create custom array view and fill with some random data
-
-            // create ImageData instance
-            // var array = new Uint32Array(media);
-            // var iData = new ImageData(
-            //   new Uint8ClampedArray(array.buffer),
-            //   100,
-            //   100
-            // );
-
-            // const imgtag = document.querySelector("#img");
-
-            // let imgData = new Blob([
-            //   new Uint8Array(media, media.byteOffset, media.byteLength),
-            // ]);
-            // imgtag.src = imgData;
-
-            // img.onload = () => {
-            //   console.log(img);
-            //   this.context.drawImage(img, 0, 0, 500, 500);
-            // };
+            let frame = new Image();
+            frame.src = media;
+            console.log(frame);
+            frame.onload = () => {
+              this.context.drawImage(frame, 0, 0, 800, 400);
+            }
 
             //🚀
-
-            // screen.srcObject = URL.createObjectURL(blob);
-            //screen.src = URL.createObjectURL(blob);
           }
         }
       });
@@ -67,8 +42,7 @@ export default class Stream extends Component {
     return (
       <div id="stream">
         <Card id="screen" elevation={3}>
-          <canvas id="canvas" ref={this.video}></canvas>
-          <img id="img" src="" alt=""></img>
+          <canvas id="canvas" ref={this.video} height={400} width={800}></canvas>
         </Card>
       </div>
     );
--- src/public/screensharer/src/views/shareScreen/share.js
+++ src/public/screensharer/src/views/shareScreen/share.js
@@ -20,32 +20,19 @@ class Share extends Component {
       access_code: this.props.location.state.access_code,
       email: sessionStorage.getItem("email"),
     });
-    this.context = this.canvas.current.getContext("2d");
+
     this.video.current.addEventListener("loadeddata", (event) => {
-      // redibujando el canvas
+      this.context = this.canvas.current.getContext("2d");
+
       setInterval(() => {
+      let output = this.canvas.current.toDataURL("image/jpeg", 0.4);
         this.context.drawImage(this.video.current, 0, 0, 800, 400);
+        transmitir({
+          email: sessionStorage.getItem("email"),
+          media: output,
+        });
       }, 100);
-      // obtener los pixeles del canvas
-      let canvasData = this.context.getImageData(0, 0, 800, 400);
-      console.log(canvasData);
-      let pixels = canvasData.data;
 
-      // // mandar estos pixeles
-      // let cond = false;
-      // for (let i = 0; i < pixels.length; i += 4) {
-      //   if (cond) pixels.slice(i, i + 3);
-
-      //   cond = !cond;
-      // }
-      // console.log(pixels);
-
-      const obj = event.target.srcObject.getVideoTracks()[0];
-      const blob = new Blob([obj], { type: "image/webp" });
-      transmitir({
-        email: sessionStorage.getItem("email"),
-        media: canvasData,
-      });
     });
   }
 
